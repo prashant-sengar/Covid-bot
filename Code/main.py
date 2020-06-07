f21619ec1b44b964934eafb2ba882aa31b243bb5
Prashant Sengar <prashantsengar812@gmail.com>
2020-06-07 17:03:22 +0530

    Updated main

--- Code/main.py
+++ Code/main.py
@@ -1,43 +1,57 @@
-import urllib
-import json
 import os
 from flask import (Flask,request, make_response)
 import sms
+from firedb import db_access
 
 # Flask app should start in global layout
 app = Flask(__name__)
 
-
-# whenever you make request /webhook
-# Following calls are made
-# webhook ->
-# -----------> Process requests
-# ---------------------------->get_data()
-
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
-
     if request.method == "POST":
         req = request.get_json(silent=True, force=True)
         res = processRequest(req)
-
         res = json.dumps(res, indent=4)
         r = make_response(res)
         r.headers['Content-Type'] = 'application/json'
         return r
 
-
 def processRequest(req):
-   
+
+    query_response = req["queryResult"]
+    params=query_response.get('parameters')
+    city=params.get('geo-city')
+    id=int(params.get('number')) 
+    print(city,'\n',id)   
+    if(city!=None):
+        res=send_text(city)
+    if(id!=None):
+        res=db_access(id)
+    #res = get_data()
+    return res
+
+
+def db(id):
+    res=db_access(id)
+    print(res,'\n')
+    if(res=='-1'):
+        return {
+            "fulfillmentText": "Doctor doesn't exist"
+        }
+    else:
+        return {
+            "fulfillmentText": "Welcome Dr."+res+". Please enter Patient ID"
+        }       
    # query_response = req["queryResult"]
     sms.text()    
-    
+
     res = get_data()
 
     return res
 
 
+def send_text(city):
+    sms.text(city)
 def get_data():
     speech = "Your appointment has been made. Please check your texts."
 
